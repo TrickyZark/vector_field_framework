c287c3728657f7f3ac70ac2a95c843450e4b32e7
TrickyZark <30292787+TrickyZark@users.noreply.github.com>
2018-10-24 20:04:40 +0800

    Create segment.java

---
+++ nc_producer/segment.java
@@ -0,0 +1,14 @@
+package read;
+
+
+public class segment{
+        private Array aub;
+        private Array avb;
+        private String topic;
+        private long i;
+        private Producer<String, byte[]> procuder;
+        private float[] float_p;
+        private int[] int_p;
+       
+
+}
